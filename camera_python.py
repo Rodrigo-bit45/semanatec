9946c79b2e5ca8dbdc90d6df0f748d873f9a0f9b
ACMR1009 <anacristina.munguia@gmail.com>
2021-05-06 13:17:51 -0600

    Add equivalent for switch case (filter menu)

--- camera_python.py
+++ camera_python.py
@@ -9,12 +9,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Camera visualization')
 
     ### Positional arguments
-    parser.add_argument('-i', '--cameraSource', default=0, help="Introduce number or camera path, default is 0 (default cam)")
-    parser.add_argument('-m', '--paramFiltro', default=5, type=int, help="Introduce number value to affect blurryness")
-
+    parser.add_argument('-cam', '--cameraSource', default=0, help="Introduce number or camera path, default is 0 (default cam)")
+    parser.add_argument('-fb', '--paramFiltroBorroso', default=5, type=int, help="Introduce number value to affect blurryness")
+    parser.add_argument('-menu', '--filterMenu', default=0, type=int, help="Introduce filter number")
     
     args = vars(parser.parse_args())
-    blurNum = int(args["paramFiltro"])
+    blurNum = int(args["paramFiltroBorroso"])
+    menuNum = int(args["filterMenu"])
 
 
     cap = cv2.VideoCapture(args["cameraSource"]) #0 local o primary camera
@@ -28,18 +29,20 @@ if __name__ == '__main__':
         if img is None:
             break
             
-        kernel = np.array([[0.272, 0.534, 0.131],
+        #"Switch case" for filter menu
+        if menuNum == 0:
+            blur = cv2.GaussianBlur(img, (blurNum,blurNum), 0)
+            cv2.imshow("Tu borroso", blur)
+        elif menuNum == 1:
+            kernel = np.array([[0.272, 0.534, 0.131],
                        [0.349, 0.686, 0.168],
                        [0.393, 0.769, 0.189]])
-        sepia = cv2.filter2D(img, -1, kernel)
-        
-        blur = cv2.GaussianBlur(img, (blurNum,blurNum), 0)
-
-        im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        cv2.imshow("Tu en blanco y negro", im_gray)
-        cv2.imshow("Tu borroso", blur)
-        cv2.imshow("Tu en sepia",sepia)
+            sepia = cv2.filter2D(img, -1, kernel)
+            cv2.imshow("Tu en sepia",sepia)
+        elif menuNum == 2:
+            im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imshow("Tu en blanco y negro", im_gray)
+       
 
         k = cv2.waitKey(10)
         if k==27:
